59caab5c39f53216fab53b29cac55dc5fd905fdc
Олег <skorikole@gmail.com>
2023-07-19 11:10:57 +0900

    added info about jUnit+comments to annotations

---
+++ src/test/java/guru/qa/FirstClass.java
@@ -0,0 +1,40 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class FirstClass {
+
+    @BeforeAll //аннотация для запуста теста до все тестов
+    static void beforeAll() { //используется метод static
+        System.out.println("beforeAll()");
+        Configuration.browser = "chrome"; //указание на конкретный запуск браузера
+    }
+
+    @AfterAll    //аннотация для запуска теста после всех тестов
+    static void afterAll() {    //используется метод static
+        System.out.println("afterAll()");
+    }
+
+    @BeforeEach
+        //аннотация запука действий для каждого теста до их запуска
+    void beforeEach() {
+        open("https://www.google.com/");
+        System.out.println("    beforeEach()");
+    }
+
+    @AfterEach
+        //аннотация запука действий для каждого теста после их запуска
+    void afterEach() {
+        open("https://www.google.com/");
+        System.out.println("    afterEach()");
+    }
+
+
+    @Test
+    void firstTest() {
+        System.out.println("        firstTest()");
+    }
+}
