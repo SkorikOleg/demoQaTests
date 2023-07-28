d788a779ddc1ab4ca742cbc63e031d17dd8ab213
Олег <skorikole@gmail.com>
2023-07-28 15:26:45 +0900

    added new test for HW2

---
+++ src/test/java/selenide/hw/SelenideHW2Test.java
@@ -0,0 +1,37 @@
+package selenide.hw;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideHW2Test {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void useHoverToGit() {
+        open("https://github.com/");
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $(".application-main").shouldHave(text("Build like the best"));
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //$("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").dragAndDrop(to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
