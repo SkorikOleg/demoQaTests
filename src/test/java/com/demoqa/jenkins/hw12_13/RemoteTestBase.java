a199063f424ee456b2d3660fa23aef76be62791c
Олег <skorikole@gmail.com>
2023-09-03 17:01:55 +0900

    fixes

--- src/test/java/com/demoqa/jenkins/hw12_13/RemoteTestBase.java
+++ src/test/java/com/demoqa/jenkins/hw12_13/RemoteTestBase.java
@@ -25,7 +25,8 @@ public class RemoteTestBase {
         Configuration.browserVersion = System.getProperty("browserVersion");
 
         String selenoidUrl = System.getProperty("selenoidUrl", "selenoid.autotests.cloud/");
-        Configuration.remote = "https://user1:1234@" + selenoidUrl + "/wd/hub";
+        String selenoidCred = System.getProperty("selenoidCred", "user1:1234@");
+        Configuration.remote = "https://" + selenoidCred + selenoidUrl + "/wd/hub";
         //для того что бы прописать креды, нужно добавить в урл https://наименование пользака:пароль@урл
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
